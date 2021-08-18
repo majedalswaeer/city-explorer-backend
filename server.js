75502c5d16f68f9d0af3ccd5ba0d2a8a9f03d8dc
majedalswaeer <majedalswaeer69@gmail.com>
2021-08-18 16:24:36 +0300

    fix server

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ const axios = require('axios')
 
 const PORT = process.env.PORT;
 
+const weatherData = require('./data/weather.json')
 server.use(cors())
 
 //___________________________________________________________________________USUAL FUNCTIONS
 
 server.get('/getWeatherBitData', getResults)
 
-server.get('/movies',getMovie)
+server.get('/movies', getMovie)
 
 server.get('/retriveData', getInfo)
 
 server.get('/', (req, res) => {
     res.send('Home Route')
 })
-
 
 server.get('/test', (req, res) => {
     res.send('It\'s Working!!')
@@ -35,7 +35,6 @@ server.get('/*', (req, res) => {
     res.send('Write another city')
 })
 
-
 server.listen(PORT, () => {
     console.log(`Listning on PORT ${PORT}`)
 })
@@ -43,25 +42,23 @@ server.listen(PORT, () => {
 //___________________________________________________________________________FUNCTIONS FOR LAB08> WEATHER AND MOVIES
 
 
-
 //** WEATHER FUNCTION AND CLASS**
 
 class WeatherBit {
     constructor(element) {
-        this.des = element.weather.description;
-        this.minTemp = element.low_temp;
-        this.maxTemp = element.max_temp;
-        this.date = element.datetime
+        this.description = element.weather.description;
+        this.low_temp = element.low_temp;
+        this.max_temp = element.max_temp;
+        this.datetime = element.datetime
     }
 }
 
-
 //localhost:3001/getWeatherBitData?city=amman
 
-function getResults(req,res) {
-    let cityWeatherBit = req.query.cityWeatherBit;
+function getResults(req, res) {
+    let sQuery = req.query.sQuery;
 
-    let weatherBitDataUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityWeatherBit}&key=${process.env.WEATHER_API_KEY}&days=4`;
+    let weatherBitDataUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${sQuery}&key=${process.env.WEATHER_API_KEY}&days=4`;
     try {
         //build a class then do the below
         axios.get(weatherBitDataUrl).then((weatherBitResults) => {
@@ -77,14 +74,11 @@ function getResults(req,res) {
             console.log('hereeee', newweatherBitOb)
             res.send(newweatherBitOb)
         });
-
     }
-
     catch (error) {
         // console.log('wtf', error)
-        res.send('Check you code :(',error);
+        res.send('Check you code :(', error);
     }
-
 }
 
 
@@ -92,22 +86,23 @@ function getResults(req,res) {
 
 class Movie {
     constructor(element) {
-       this.title=element.original_title
-       this.overview=element.overview
-       this.avgVotes=element.vote_average
-       this.imgUrl=`https://image.tmdb.org/t/p/original/${element.poster_path}`
-       this.popularity=element.popularity
-       this.release=element.release_date
+        this.original_title = element.original_title
+        this.overview = element.overview
+        this.vote_average = element.vote_average
+        this.poster_path = `https://image.tmdb.org/t/p/original${element.poster_path}`
+        this.popularity = element.popularity
+        this.release_date = element.release_date
     }
 }
+//http://localhost:3001/movies?sQuery=amman
+function getMovie(req, res) {
+    let sQuery = req.query.sQuery;
 
-function getMovie(req,res){
-    let cityMovie = req.query.cityMovie;
-
-    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_KEY}&query=${cityMovie}&language=en-US`;
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_KEY}&query=${sQuery}&language=en-US`;
 
     console.log(movieUrl)
     try {
+
         //build a class then do the below
         axios.get(movieUrl).then((movieResults) => {
 
@@ -119,36 +114,34 @@ function getMovie(req,res){
 
                 return new Movie(item);
             });
-            console.log('hereeee', newMovieOb )
+            console.log('hereeee', newMovieOb)
             res.send(newMovieOb)
         });
-
     }
 
     catch (error) {
         console.log('wtf', error)
         // res.send('Check you code :(',error);
     }
-
-
 }
+
 
 //___________________________________________________________________________ FUNCTION FOR LAB07
 
 
-function getInfo(req,res){
-    let city = req.query.cityName;
+function getInfo(req, res) {
+    let sQuery = req.query.sQuery;
 
     let newData = weatherData.find(element => {
 
-        if (element.city_name.toLowerCase() === city.toLowerCase()) {
+        if (element.city_name.toLowerCase() === sQuery.toLowerCase()) {
             return element;
         }
     })
 
     res.send(newData);
 }
-    
+
 
 
 
