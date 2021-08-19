6c7c0af16f37cce4e145ae6e26ca2dfc9a7365eb
majedalswaeer <majedalswaeer69@gmail.com>
2021-08-19 14:48:40 +0300

    lab10 is done

--- movies.js
+++ movies.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 
+let allreadyMovieSaved= {};
 
 class Movie {
     constructor(element) {
@@ -16,30 +17,32 @@ function getMovie(req, res) {
     let sQuery = req.query.sQuery;
 
     let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_KEY}&query=${sQuery}&language=en-US`;
+    if (allreadyMovieSaved[sQuery] !== undefined){
+        console.log('saved')
 
-    console.log(movieUrl)
-    try {
+        res.send(allreadyMovieSaved[sQuery])
+    } else{
+        try {
 
-        //build a class then do the below
-        axios.get(movieUrl).then((movieResults) => {
-
-            // console.log('results', movieResults.data.results)
-
-            let newMovieOb = movieResults.data.results.map(item => {
-
-                // console.log('ewewewene', movieResults.data.results)
-
-                return new Movie(item);
+            //build a class then do the below
+            axios.get(movieUrl).then((movieResults) => {
+    
+                let newMovieOb = movieResults.data.results.map(item => {
+    
+                    return new Movie(item);
+                });
+                allreadyMovieSaved[sQuery]=newMovieOb
+                res.send(newMovieOb)
             });
-            console.log('hereeee', newMovieOb)
-            res.send(newMovieOb)
-        });
-    }
+        }
+    
+        catch (error) {
+            console.log('wtf', error)
+            // res.send('Check you code :(',error);
+        }
 
-    catch (error) {
-        console.log('wtf', error)
-        // res.send('Check you code :(',error);
     }
+    
 }
 
 module.exports=getMovie
--- weatherBit.js
+++ weatherBit.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 
+let allreadySaved= {};
+
 //** WEATHER FUNCTION AND CLASS**
 
 class WeatherBit {
@@ -17,25 +19,34 @@ function getResults(req, res) {
     let sQuery = req.query.sQuery;
 
     let weatherBitDataUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${sQuery}&key=${process.env.WEATHER_API_KEY}&days=4`;
-    try {
-        //build a class then do the below
-        axios.get(weatherBitDataUrl).then((weatherBitResults) => {
 
-            // console.log('results', weatherBitResults.data.data)
+    if (allreadySaved[sQuery] !== undefined){
+        console.log('saved')
 
-            let newweatherBitOb = weatherBitResults.data.data.map(item => {
+        res.send(allreadySaved[sQuery])
+    } else{
+        try {
+            //build a class then do the below
+            
+    
+            axios.get(weatherBitDataUrl).then((weatherBitResults) => {
+    
 
-                console.log('ewewewene', weatherBitResults.data.data)
-
-                return new WeatherBit(item);
+    
+                let newweatherBitOb = weatherBitResults.data.data.map(item => {
+    
+                    return new WeatherBit(item);
+                });
+                console.log('notsaved')
+                allreadySaved[sQuery]=newweatherBitOb
+                res.send(newweatherBitOb)
             });
-            console.log('hereeee', newweatherBitOb)
-            res.send(newweatherBitOb)
-        });
+        }
+        catch (error) {
+            res.send('Check you code :(', error);
+        }
+
     }
-    catch (error) {
-        // console.log('wtf', error)
-        res.send('Check you code :(', error);
-    }
+    
 }
 module.exports=getResults
